2b801b168074926679dd72fcee743ab4e496951c
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-15 19:50:20 +0100

    section 16 Form

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -25,6 +25,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
       );
   }
 
+  onEditItem(index: number) {
+
+  }
+
   ngOnDestroy(): void {
     this.igChangeSub.unsubscribe();
   }
